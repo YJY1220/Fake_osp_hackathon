fd7c774c494c0d3ec383f11061a7fdc93a0286cb
YJY1220 <jiyeyu1220@gmail.com>
2022-06-04 18:28:38 +0900

    app.py

--- fakevenvs/fakeproject/fake/app.py
+++ fakevenvs/fakeproject/fake/app.py
@@ -5,11 +5,11 @@ from flask import Flask, render_template, url_for, request, json
 import requests
 import re
 from bs4 import BeautifulSoup
-from elasticsearch import Elasticsearch
-from elasticsearch import helpers
+# from elasticsearch import Elasticsearch
+# from elasticsearch import helpers
  
 app = Flask(__name__)
-es_host = "http://localhost:9200"
+#es_host = "http://localhost:9200"
 
 @app.route('/')
 def main():
@@ -29,7 +29,7 @@ def crawling():
             exxxx_list=[]
             # content = soup.select_one('ul.list_jobs')
             # titles = content.select('li >div > div > a')
-            titles = soup.find_all('div', {'class' : 'wrap_info'})
+            titles = soup.find_all('h4', {'class' : 'tit_jobs'})
             for i in titles:
                 if(i==None):
                     break
@@ -44,12 +44,32 @@ def crawling():
 
                 if len(i) > 0:
                     result_list.append(i)
-            es = Elasticsearch(es_host)
+            
+            another = soup.find_all('a', {'class' : 'link_tag'})
+            atat_list=[]
+            rrrr_list=[]
+            final_list=[]
+            for r in another:
+                if(r==None):
+                    break
+                atat_list.append(r.get_text())
+            for r in atat_list:
+                rrrr_list.append(r.lower())
+            
+            for r in rrrr_list:
+                r = re.sub('\n','',r)
+                r = re.sub('\n','',r)
+                r = r.split('[')
 
-            dic = []
-            for i in result_list:
-                dic.append({"word" : i})
-                res = es.
+                if len(r) > 0:
+                    final_list.append(r)
+
+            #es = Elasticsearch(es_host)
+
+            # dic = []
+            # for i in result_list:
+            #     dic.append({"word" : i})
+                #res = es.
             # for title in titles:
             #     if(title==None):
             #         break
@@ -146,8 +166,76 @@ def crawling():
         else:
             print(response.status_code)
 
-    return render_template('crawling.html', response = result_list)
+    return render_template('crawling.html', response = result_list, allres = final_list)
 
 
 if __name__=="__main__":
     app.run()
+
+
+# #!usr/bin/python3
+# # -*- coding: utf-8 -*-
+
+# import re
+# import requests
+# from bs4 import BeautifulSoup
+# from flask import Flask, render_template, request, json
+# from elasticsearch import Elasticsearch
+
+# app = Flask(__name__)
+# es_host = "http://127.0.0.1:9200"
+
+
+# @app.route('/', methods=['GET'])
+# def home():
+#     return render_template('home.html')
+
+
+# @app.route('/crawl')  # , methods=['GET', 'POST'])
+# def crawl():
+#     if request.method == 'GET':
+#         url = request.args.get("url")
+
+#         if url == 'https://en.wikipedia.org/wiki/Web_crawler ' or url == 'https://en.wikipedia.org/wiki/NoSQL':
+#             page = requests.get(url)
+
+#             soup = BeautifulSoup(page.content, 'html.parser')
+
+#             tb = soup.find_all('p')
+#             tb = tb + soup.find_all('span', class_="mw-headline")
+#             tb = tb + soup.find_all('cite', class_="citation")
+
+#             s = ""
+#             for t in tb:
+#                 s = s + t.get_text()
+
+#             s = re.sub("<>.", '', s, 0).strip()
+#             s = re.sub('[-=+,#/\?:;^.@*\"※~ㆍ!‘|\(\)\[\]`\'…\”\“\’·  ]', ' ', s)
+
+#             list = s.split()
+#             wordList = []
+#             countList = []
+
+#             for i in list:
+#                 if i in wordList:
+#                     countList[wordList.index(i)] = countList[wordList.index(i)] + 1
+#                 else:
+#                     wordList.append(i)
+#                     countList.append(1)
+
+#             es = Elasticsearch(es_host)
+
+#             dic = []
+#             for i in wordList:
+#                 dic.append({"url": url, "word": i, "frequency": str(countList[wordList.index(i)])})
+#                 res = es.index(index="web", id=wordList.index(i), document=dic[wordList.index(i)])
+
+#             return render_template('resultpage.html', link=url, wordlist=wordList, countlist=countList)
+#         else:
+#             return "this URL is not Supporting"
+#     else:
+#         return "this URL is not Supporting"
+
+
+# if __name__ == "__main__":
+#     app.run(debug=True)
