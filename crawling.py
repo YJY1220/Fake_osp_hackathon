7ac39d53d882733117d0245e4a2d4449c1ff833a
YJY1220 <jiyeyu1220@gmail.com>
2022-06-21 04:29:03 +0900

    kakao elastic data insert complete

--- crawling.py
+++ crawling.py
@@ -134,19 +134,37 @@ import pprint
 from bs4 import BeautifulSoup
 from flask import Flask, render_template, request
 from elasticsearch import Elasticsearch
-
+from elasticsearch import helpers
 es = Elasticsearch('http://localhost:9200')
 
-field = ["tech", "design", "service", "client"]
-company = ["c1", "c2", "c3"]
-job_dic = {"c1":["job1", "job2", "job3"], "c2":["job4", "job5"]}
 
 # index랑 doc_type차이점
 
-for job in job_dic.keys():
-    print(job)
-    print(job_dic[job])
+ 
+index = 'employment'
+doc_type = 'kakao'
+data = {
+    'title': '',
+    'tags': '',
+    'url': ''
+    }
+
+def insert(body):
+    return es.index(index=index, body=body)
     
+# def search(index, data=None):
+#     if data is None:
+#         data = {"match_all": {}}
+#     else:
+#         data = {"match": data}
+    
+#     body = {"query": data}
+#     res = es.search(index=index, body=body)
+#     return res
+
+
+
+
 kakao_url = "https://careers.kakao.com/jobs"
 res_dic = {}
 link_dic = {}
@@ -166,34 +184,14 @@ for i in range(1, 7):
         res_dic[title] = tags
         link_dic[title] = link
 
-def insert(body):
-    return es.index(index=index, body=body)
-    
-def search(index, data=None):
-    if data is None:
-        data = {"match_all": {}}
-    else:
-        data = {"match": data}
-    
-    body = {"query": data}
-    res = es.search(index=index, body=body)
-    return res
-
-index = 'employment'
-doc_type = 'kakao'
-data ={
-    'title': '',
-    'tags': '',
-    'url': ''
-}
 
 for job in res_dic.keys():
     data['title'] = job
     data['tags'] = res_dic[job]
     data['url'] = link_dic[job]
-    ir = insert(data)
-    
-    print(ir)
+    #print(data)
+    ir =insert(data)
+    pprint.pprint(ir)    
 
-sr = search(index)
-pprint.pprint(sr)
+#sr = es.search(data, index="employment") 
+
