f0a9b7c9ac14e82621b7ea9c90ebe95883e876dd
YJY1220 <jiyeyu1220@gmail.com>
2022-06-20 22:56:12 +0900

    app.py

--- osp_final/OSP09_Final_Project/projects/app.py
+++ osp_final/OSP09_Final_Project/projects/app.py
@@ -7,7 +7,8 @@ from bs4 import BeautifulSoup
 from flask import Flask, render_template, request
 import time
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
 app = Flask(__name__)
 
@@ -45,7 +46,7 @@ def contents():
 
         if (company.__eq__("naver")):
             url = "https://recruit.navercorp.com/rcrt/list.do?srchClassCd=1000000"
-            driver = webdriver.Chrome('C:\chromedriver_win32\chromedriver.exe')
+            driver = webdriver.Chrome(executable_path = 'C:\\chromedriver_win32\\chromedriver')
             driver.get(url)
             driver.implicitly_wait(5)
 
@@ -94,7 +95,7 @@ def contents():
 
             for job in soup.find('ul', class_="job_list").find_all('li'):
                 link = "https://careers.linecorp.com/" + job.find('a')["href"]
-                title = job.find('h4', class_='tit_jobs').text
+                title = job.find('h3', class_='title').text
                 tag = job.find_all('div', class_='text_filter')
                 tags = []
                 for item in tag:
@@ -104,6 +105,82 @@ def contents():
                 link_dic[title] = link
 
             return render_template('line.html', result=res_dic, link=link_dic)
+
+        if (company.__eq__("coupang")):
+            url = 'https://www.coupang.jobs/kr/jobs/?department=Ecommerce+Engineering&department=Play+Engineering&department=Product+UX&department=Search+and+Discovery&department=Search+and+Discovery+Core+Infrastructure&department=Cloud+Platform&department=Corporate+IT&department=eCommerce+Product&department=FTS+(Fulfillment+and+Transportation+System)&department=Marketplace%2c+Catalog+%26+Pricing+Systems&department=Program+Management+Office&department=Customer+Experience+Product'
+            req = requests.get(url)
+            soup = BeautifulSoup(req.text, 'html.parser')
+
+            list_area = soup.find_all('div', class_='card-body')            
+            link_dic = {}
+            
+            for job in list_area:
+                link = "https://www.coupang.jobs" + job.a["href"]
+                title = job.find_all('a', class_='stretched-link')
+                for text in title:
+                    title_text = text.get_text()
+
+                link_dic[title_text] = link
+
+            return render_template('coupang.html', link=link_dic)
+
+        if (company.__eq__("woowahan")):
+            url = "https://career.woowahan.com/?jobCodes&employmentTypeCodes=&serviceSectionCodes=&careerPeriod=&keyword=&category=jobGroupCodes%3ABA005001#recruit-list"
+            options = webdriver.ChromeOptions()
+            options.add_experimental_option("excludeSwitches", ["enable-logging"])
+            driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),options=options)
+            # driver = webdriver.Chrome(executable_path = 'C:\\chromedriver_win32\\chromedriver',options=options)
+
+            driver.get(url)
+            driver.implicitly_wait(5)
+
+            last_height = driver.execute_script("return document.body.scrollHeight")
+            while True:
+                driver.execute_script("window.scrollTo(0, document.documentElement.scrollHeight);")
+                time.sleep(0.5)
+                new_height = driver.execute_script("return document.documentElement.scrollHeight")
+
+                if new_height == last_height:
+                   break
+                else:
+                   last_height = new_height
+
+            new_height = last_height
+
+            html = driver.page_source
+            soup = BeautifulSoup(html, 'html.parser')
+
+            res_dic = {}
+            link_dic = {}
+            list_area = soup.find('ul', class_="recruit-type-list")
+            for job in list_area.find_all('li', class_=False):
+                link = "https://career.woowahan.com" + job.a["href"]
+                title = job.find('p', class_='fr-view').text
+                tag = job.find_all('div', class_='flag-tag')                
+                tags = []
+                for item in tag:
+                    tags.append(item.get_text().strip())
+                res_dic[title] = tags
+                link_dic[title] = link
+
+            return render_template('woowahan.html', result=res_dic, link=link_dic)
+        
+        if(company.__eq__('daangn')):
+            url = 'https://team.daangn.com/jobs/'
+            req = requests.get(url)
+            soup = BeautifulSoup(req.text, 'html.parser')
+
+            list_area = soup.find('div', class_=False)            
+            link_dic = {}
+            
+            for job in list_area.find_all('li', class_="c-deAcZv"):
+                link = "https://team.daangn.com/jobs" + job.a["href"]
+                title = job.find_all('h3', class_='c-boyXyq')
+                for text in title:
+                    title_text = text.get_text()
+                    link_dic[title_text] = link
+
+            return render_template('daangn.html',link=link_dic)
 
 if __name__=='__main__':
     app.run()
