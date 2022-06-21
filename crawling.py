f1443b94fcb5d55a4ee22eea789f1d27eec3847d
YJY1220 <jiyeyu1220@gmail.com>
2022-06-21 10:07:43 +0900

    example

--- crawling.py
+++ crawling.py
@@ -2,39 +2,46 @@ import requests
 from bs4 import BeautifulSoup
 import time
 import re
-import pprint
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
-from elasticsearch import Elasticsearch
-from elasticsearch import helpers
 
-es = Elasticsearch('http://localhost:9200')
+# kakao_url = "https://careers.kakao.com/jobs"
+# naver_url = "https://recruit.navercorp.com/cnts/tech"
+# line_url = "https://careers.linecorp.com/ko/jobs?ca=Engineering&ci=Seoul,Bundang&co=East%20Asia"
+# coupang_url = "https://www.coupang.jobs/kr/jobs/?department=Ecommerce+Engineering&department=Play+Engineering&department=Product+UX&department=Search+and+Discovery&department=Search+and+Discovery+Core+Infrastructure&department=Cloud+Platform&department=Corporate+IT&department=eCommerce+Product&department=FTS+(Fulfillment+and+Transportation+System)&department=Marketplace%2c+Catalog+%26+Pricing+Systems&department=Program+Management+Office&department=Customer+Experience+Product"
+# woowahan_url = "https://career.woowahan.com/?jobCodes&employmentTypeCodes=&serviceSectionCodes=&careerPeriod=&keyword=&category=jobGroupCodes%3ABA005001#recruit-list"
+# daangn_url = "https://team.daangn.com/jobs/"
+# url = 'https://team.daangn.com/jobs/'
+# req = requests.get(url)
+# soup = BeautifulSoup(req.text, 'html.parser')
 
+# list_area = soup.find('div', class_=False)            
+# link_dic = {}
+            
+# for job in list_area.find_all('li', class_="c-deAcZv"):
+#     link = "https://team.daangn.com/jobs" + job.a["href"]
+#     title = job.find_all('h3', class_='c-boyXyq')
+#     for text in title:
+#         title_text = text.get_text()
 
-# index랑 doc_type차이점
+#         link_dic[title_text] = link
 
- 
-index = 'employment'
-doc_type = 'daangn'
-data = {
-    'title': '',
-    'tags' : '',
-    'url': ''
-    }
+# print(link_dic)
 
-def insert(body):
-    return es.index(index=index, body=body)
-
-kakao_url = "https://careers.kakao.com/jobs"
-naver_url = "https://recruit.navercorp.com/cnts/tech"
-line_url = "https://careers.linecorp.com/ko/jobs?ca=Engineering&ci=Seoul,Bundang&co=East%20Asia"
-coupang_url = "https://www.coupang.jobs/kr/jobs/?department=Ecommerce+Engineering&department=Play+Engineering&department=Product+UX&department=Search+and+Discovery&department=Search+and+Discovery+Core+Infrastructure&department=Cloud+Platform&department=Corporate+IT&department=eCommerce+Product&department=FTS+(Fulfillment+and+Transportation+System)&department=Marketplace%2c+Catalog+%26+Pricing+Systems&department=Program+Management+Office&department=Customer+Experience+Product"
-woowahan_url = "https://career.woowahan.com/?jobCodes&employmentTypeCodes=&serviceSectionCodes=&careerPeriod=&keyword=&category=jobGroupCodes%3ABA005001#recruit-list"
-daangn_url = "https://team.daangn.com/jobs/"
 
 url = "https://career.woowahan.com/?jobCodes&employmentTypeCodes=&serviceSectionCodes=&careerPeriod=&keyword=&category=jobGroupCodes%3ABA005001#recruit-list"
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+options = webdriver.ChromeOptions()
+#options = webdriver.ChromeOptions()
+options.add_experimental_option("excludeSwitches", ["enable-logging"])
+options.add_argument('--headless')
+#chrome_options.add_argument('--no-sandbox')
+# chrome_options.add_argument("--single-process")
+# chrome_options.add_argument('--disable-dev-shm-usage')
+path = "/home/jiye/.wdm/drivers/chromedriver/linux64/102.0.5005.61/chromedriver"
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+#driver = webdriver.Chrome(path, chrome_options=chrome_options)
+
 driver.get(url)
 driver.implicitly_wait(5)
 
@@ -49,11 +56,10 @@ while True:
     else:
         last_height = new_height
 
-new_height = last_height
+    new_height = last_height
 
 html = driver.page_source
 soup = BeautifulSoup(html, 'html.parser')
-
 
 res_dic = {}
 link_dic = {}
@@ -61,18 +67,11 @@ list_area = soup.find('ul', class_="recruit-type-list")
 for job in list_area.find_all('li', class_=False):
     link = "https://career.woowahan.com" + job.a["href"]
     title = job.find('p', class_='fr-view').text
-    tag = job.find_all('div', class_='flag-tag')
+    tag = job.find_all('div', class_='flag-tag')                
     tags = []
     for item in tag:
         tags.append(item.get_text().strip())
-
     res_dic[title] = tags
     link_dic[title] = link
 
-
-for job in link_dic.keys():
-    data['title'] = job
-    data['tags'] = link_dic[job]
-    data['url'] = link_dic[job]
-    ir =insert(data)
-
+print(res_dic)
